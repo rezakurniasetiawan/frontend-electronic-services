d0ecb81ed8e274efd70eaa9594ea95638536fbaa
rezakurniasetiawan <56224328+rezakurniasetiawan@users.noreply.github.com>
2025-02-24 17:54:46 +0700

    update

--- app/Http/Controllers/landingpageController.php
+++ app/Http/Controllers/landingpageController.php
@@ -12,6 +12,7 @@ class landingpageController extends Controller
         $about = \App\Models\about::first();
         $services = \App\Models\service::all();
         $testimonials = \App\Models\testimoni::all();
+        $contact = \App\Models\setting::first();
         return view('landingpage.index', compact('carousel', 'about', 'services', 'testimonials'));
     }
 }
--- resources/views/landingpage/sections/contact.blade.php
+++ resources/views/landingpage/sections/contact.blade.php
@@ -7,10 +7,8 @@
     </div><!-- End Section Title -->
 
     <div class="container" data-aos="fade-up" data-aos-delay="100">
-        <iframe
-            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3951.215503549693!2d112.607692675007!3d-7.97666499204853!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zN8KwNTgnMzYuMCJTIDExMsKwMzYnMzcuMCJF!5e0!3m2!1sid!2sid!4v1737704236002!5m2!1sid!2sid"
-            style="border:0; width: 100%; height: 370px;" class="mb-4" allowfullscreen="" loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src={{ $contact->embed_map }} style="border:0; width: 100%; height: 370px;" class="mb-4"
+            allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
         <div class="row gy-4">
             <div class="col-lg-12 ">
@@ -20,7 +18,7 @@
                             data-aos="fade-up" data-aos-delay="200">
                             <i class="bi bi-geo-alt"></i>
                             <h3>Alamat</h3>
-                            <p>Jl Pisang Candi Barat No 17b Kec.sukun Kab.Malang</p>
+                            <p>{{ $contact->address }}</p>
                         </div>
                     </div><!-- End Info Item -->
 
@@ -29,7 +27,7 @@
                             data-aos="fade-up" data-aos-delay="400">
                             <i class="bi bi-envelope"></i>
                             <h3>Email</h3>
-                            <p>info@example.com</p>
+                            <p>{{ $contact->email }}</p>
                         </div>
                     </div><!-- End Info Item -->
 
